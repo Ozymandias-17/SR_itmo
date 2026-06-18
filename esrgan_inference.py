e65857dac7a45d793250edb37fd7e27991ae5eb9
Oleg Arzumanov <oarzumanov17@gmail.com>
2026-06-18 03:08:39 +0300

    code improvement

--- esrgan_inference.py
+++ esrgan_inference.py
@@ -19,44 +19,50 @@ from dataloader import DF2KDataset
 
 def validate():
     parser = argparse.ArgumentParser(description='ESRGAN Testing')
-    parser.add_argument('--weights', type=str, default='./checkpoints/best_esrgan.pth', 
-                        help='Путь к весам модели')
+    parser.add_argument('--mode', type=str, default='orig', 
+                        help='Mode: orig, rgb2, yuv')
     parser.add_argument('--scale', type=int, default=2, 
-                        help='Масштаб увеличения (2 или 4)')
+                        help='Scale parametr (2 or 4)')
     parser.add_argument('--save_num', type=int, default=10, 
-                        help='Количество визуальных результатов для сохранения')
-    parser.add_argument('--output_dir', type=str, default='./validation_results', 
-                        help='Папка для сохранения визуальных результатов')
+                        help='Number of visual results to save')
+    parser.add_argument('--output_dir', type=str, default='./validation_results')
     
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"устройство: {device}")
+    print(f"Device: {device}")
 
-    model = RRDBNet(in_nc=3, out_nc=3, nf=64, nb=23).to(device)
-    
-    if not os.path.exists(args.weights):
-        raise FileNotFoundError(f"Веса не найдены по пути: {args.weights}")
-    
-    model.load_state_dict(torch.load(args.weights, map_location=device))
+    if args.mode == 'rgb2':
+        nf_base, gc_base = 32, 16
+        print(f"Mode 'rgb2': number of channels (nf и gc) is halved (nf={nf_base}, gc={gc_base}).")
+    elif args.mode == 'orig':
+        nf_base, gc_base = 64, 32
+        print(f"Mode 'orig': standart channels (nf={nf_base}, gc={gc_base}).")
+
+    model = RRDBNet(in_nc=3, out_nc=3, nf=nf_base, nb=23, gc=gc_base).to(device)
+    model.load_state_dict(torch.load(f'./checkpoints/esrgan_{args.mode}.pth', map_location=device))
     model.eval()
-    print(f"загружены веса: {args.weights}")
+    print(f"Weights are downloaded: {args.weights}")
 
     val_dataset = DF2KDataset(split='val', scale=args.scale)
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-    print(f"Найдено {len(val_loader)} изображений в валидационном сете.")
+    print(f"Found {len(val_loader)} images in validation set.")
 
     psnr_metric = PeakSignalNoiseRatio(data_range=1.0).to(device)
     ssim_metric = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
     lpips_metric = lpips.LPIPS(net='vgg').to(device)
+    lpips_metric.eval()
+    for p in lpips_metric.parameters():
+        p.requires_grad = False
+        
     dists_metric = DISTS().to(device)
+    dists_metric.eval()
+    for p in dists_metric.parameters():
+        p.requires_grad = False
 
     total_psnr, total_ssim, total_lpips, total_dists = 0.0, 0.0, 0.0, 0.0
     
     os.makedirs(args.output_dir, exist_ok=True)
-
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
 
     # Цикл валидации
     with torch.no_grad():
@@ -86,11 +92,11 @@ def validate():
     avg_dists = total_dists / num_samples
 
     print("\n")
-    print(f"РЕЗУЛЬТАТЫ ВАЛИДАЦИИ (Scale: X{args.scale})")
-    print(f"Средний PSNR:  {avg_psnr:.2f} dB")
-    print(f"Средний SSIM:  {avg_ssim:.4f}")
-    print(f"Средний LPIPS: {avg_lpips:.4f}")
-    print(f"Средний DISTS: {avg_dists:.4f}")
+    print(f"RESULTS (Scale: X{args.scale})")
+    print(f"Avg PSNR:  {avg_psnr:.2f} dB")
+    print(f"Avg SSIM:  {avg_ssim:.4f}")
+    print(f"Avg LPIPS: {avg_lpips:.4f}")
+    print(f"Avg DISTS: {avg_dists:.4f}")
 
     res = {"timestamp": datetime.now().isoformat(timespec="seconds"),
            "psnr": avg_psnr,
@@ -102,43 +108,45 @@ def validate():
     with open(res_path, "w", encoding="utf-8") as f:
         json.dump(res, f, ensure_ascii=False, indent=4)
 
-    print(f"Метрики сохранены в: {res_path}")
+    print(f"Metrics are saved in: {res_path}")
 
 
 def inference():
 
     parser = argparse.ArgumentParser(description='ESRGAN Inference')
     parser.add_argument('--input', type=str, required=True, 
-                        help='Путь к входному низкоразрешенному изображению (LR)')
+                        help='Path to LR image')
     parser.add_argument('--output', type=str, default='./results/output.png', 
-                        help='Путь, куда сохранить полученный апскейл (HR)')
-    parser.add_argument('--weights', type=str, default='./checkpoints/best_esrgan.pth', 
-                        help='Путь к сохраненным весам модели (.pth)')
+                        help='Path to save upscaled image')
+    parser.add_argument('--mode', type=str, default='orig', 
+                        help='Mode: orig, rgb2, yuv')
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Устройство: {device}")
+    print(f"Device: {device}")
 
-    model = RRDBNet(in_nc=3, out_nc=3, nf=64, nb=23).to(device)
+    if args.mode == 'rgb2':
+        nf_base, gc_base = 32, 16
+        print(f"Mode 'rgb2': number of channels (nf и gc) is halved (nf={nf_base}, gc={gc_base}).")
+    elif args.mode == 'orig':
+        nf_base, gc_base = 64, 32
+        print(f"Mode 'orig': standart channels (nf={nf_base}, gc={gc_base}).")
 
-    if not os.path.exists(args.weights):
-        raise FileNotFoundError(f"Веса не найдены по пути {args.weights}.")
-
-    state_dict = torch.load(args.weights, map_location=device)
-    model.load_state_dict(state_dict)
+    model = RRDBNet(in_nc=3, out_nc=3, nf=nf_base, nb=23, gc=gc_base).to(device)
+    model.load_state_dict(torch.load(f'./checkpoints/esrgan_{args.mode}.pth', map_location=device))
     model.eval()
 
     if not os.path.exists(args.input):
-        raise FileNotFoundError(f"Входное изображение {args.input} не найдено.")
+        raise FileNotFoundError(f"Input image {args.input} is not found.")
 
     img = Image.open(args.input).convert('RGB')
     orig_w, orig_h = img.size
-    print(f"[i] Исходное разрешение: {orig_w}x{orig_h}")
+    print(f"Original shape: {orig_w}x{orig_h}")
 
     transform = transforms.ToTensor()
     img_tensor = transform(img).unsqueeze(0).to(device) # [1, 3, H, W]
 
-    print("Запуск апскейлинга...")
+    print("Start Upscaler...")
     with torch.no_grad(): 
         output_tensor = model(img_tensor)
 
@@ -154,7 +162,7 @@ def inference():
     
     result_img = Image.open(args.output)
     new_w, new_h = result_img.size
-    print(f"Результат сохранен в: {args.output}")
+    print(f"Result is saved in: {args.output}")
 
 
 if __name__ == '__main__':
